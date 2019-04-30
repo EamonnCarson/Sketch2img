8be083719c8deb43554a990acc8346a68011e776
dongsubkim <ds.kim@berkeley.edu>
2019-04-29 21:19:00 -0700

    implemented generator

--- exploration_danny/generator.py
+++ exploration_danny/generator.py
@@ -23,8 +23,8 @@ class MRU(nn.Module):
         self.sigmoid1 = nn.sigmoid(inplace=True)
         self.mm1 = torch.mm()
         self.activation = activation # activation needs to be in nn.module
-        self.conv2 = conv3x3()
-        self.conv3 = conv3x3()
+        self.conv2 = conv3x3(None, None) # TODO :: fill channels
+        self.conv3 = conv3x3(None, None) # TODO :: fill channels
         self.sigmoid2 = nn.sigmoid(inplace=True)
         self.mm2 = torch.mm()
         self.mm3 = torch.mm()
@@ -59,7 +59,7 @@ class sketchGAN_G(nn.Moduel):
     def __init__(self, mru, layer_dims, sampling_dims):
         # layer_dims is a list of out_channels for each layer
         # sampling_dims is a list of output shape of each layer (y)
-        super(sketchGAN, self).__init__()
+        super(sketchGAN_G, self).__init__()
         self.in_channels = None # TODO :: set to initial channel
         # encoding layers
         self.enc_layer1 = self.make_layer(mru, layer_dims[0], interpoloate=sampling_dims[0])
@@ -73,19 +73,24 @@ class sketchGAN_G(nn.Moduel):
         self.dec_layer4 = self.make_layer(mru, layer_dims[7], interpoloate=sampling_dims[7])
     
     def make_layer(self, mru, out_channels, activation=nn.ReLU, stride=1, interpoloate=None):
-        layer = mru(in_channels, out_channels, activation=activation, stride=stride, interpoloate=interpoloate)
+        layer = mru(self.in_channels, out_channels, activation=activation, stride=stride, interpoloate=interpoloate)
         self.in_channels = out_channels
         return nn.Sequential(*layer)
 
     def forward(self, x):
-        out = self.enc_layer1(out)
-        out = self.enc_layer2(out)
-        out = self.enc_layer3(out)
-        out = self.enc_layer4(out)
-        out = self.dec_layer1(out)
-        out = self.dec_layer2(out)
-        out = self.dec_layer3(out)
-        out = self.dec_layer4(out)
+        out1 = self.enc_layer1(x)
+        out2 = self.enc_layer2(out1)
+        out3 = self.enc_layer3(out2)
+        out4 = self.enc_layer4(out3)
+        out5 = self.dec_layer1(out4)
+        # out5 = torch.cat((out5, out4), axis=1) 
+        # Not sure this is needed. This skip-connection is not shown in figure diagram
+        out6 = self.dec_layer2(out5)
+        out6 = torch.cat((out6, out3), axis=1)
+        out7 = self.dec_layer3(out6)
+        out7 = torch.cat((out7, out2), axis=1)
+        out = self.dec_layer4(out7)
+        out = torch.cat((out, out1), axis=1)
         return out
 
 # model = sketchGAN_G(MRU, layer_dims, sampling_dims).to(device)
