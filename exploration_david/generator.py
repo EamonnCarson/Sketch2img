c1b6640fc588877b7963e60e7cc19af55802a9ff
Chihchung Wang <chwang733@gmail.com>
2019-05-02 04:59:50 -0400

    Finish encoder for generator? Still need to implement conditional batch-norm maybe

--- exploration_david/generator.py
+++ exploration_david/generator.py
@@ -7,9 +7,17 @@ import torchvision.transforms as transforms
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class MRU(nn.Module):
+    """
+    Masked Residual Unit, which takes in feature maps x_i and an image I, and outputs new feature maps y_i
+    """
     def conv3x3(in_channels, out_channels, sn):
         """
         Implements 3x3 convolution layer in MRU unit.
+        
+        Args:
+            in_channels: Number of input channels (e.g. 3 for a RGB image)
+            out_channels: Number of output channels
+            sn: Set True to use spectral normalization (mainly for discriminator)
         """
         # Padding of 1 ensures that input size = output size
         conv = nn.Conv2d(in_channels, out_channels, 3, padding=1)
@@ -18,15 +26,15 @@ class MRU(nn.Module):
             conv = torch.nn.utils.spectral_norm(conv)
         return conv
     
-    def __init__(self, in_channels, out_channels, activation, image_channels=3, sn=False):
+    def __init__(self, in_channels, out_channels, image_channels, activation, sn=False):
         """
         Initializes a MRU unit for SketchyGAN.
         
         Args:
             in_channels: Number of input channels (e.g. 3 for a RGB image)
             out_channels: Number of output channels
-            activation: Activation function, f, to use
             image_channels: Number of channels for the input image
+            activation: Activation function, f, to use
             sn: Set True to use spectral normalization (mainly for discriminator)
         """
         super(MRU, self).__init__()
@@ -42,7 +50,7 @@ class MRU(nn.Module):
         
         Args:
             xi: Input feature map of size (batch_size, in_channels, height, width)
-            image: Input sketch of size (batch_size, sketch_channels, height, width)
+            image: Input images of size (batch_size, sketch_channels, height, width)
             
         Returns:
             yi: Output feature map of size (batch_size, out_channels, height, width)
@@ -61,13 +69,82 @@ class MRU(nn.Module):
 
 
 class EncoderBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, activation, image_channels=3):
-        self.mru = MRU(in_channels, out_channels, activation, image_channels)
+    """
+    Encoder block for generator, which includes MRU followed by downsampling
+    """
+    def __init__(self, in_channels, out_channels, image_channels, activation, norm):
+        """
+        Initializes the encoder block unit for SketchyGAN
+        
+        Args:
+            in_channels: Number of input channels (e.g. 3 for a RGB image)
+            out_channels: Number of output channels
+            image_channels: Number of channels for the input images
+            activation: Activation function, f, to use
+            norm: Normalization function to use
+        """
+        self.mru = MRU(in_channels, out_channels, image_channels, activation)
+        # Halve height and width
         self.pool = nn.Conv2d(out_channels, out_channels, kernel_size=2, stride=2)
+        self.norm = norm
     
     def forward(self, xi, image):
-        return self.pool(self.mru(xi, image))
+        return self.norm(self.pool(self.mru(xi, image)))
 
+
+class Encoder(nn.Module):
+    """
+    Encoder for generator, which feeds output into the decoder
+    """
+    def __init__(self, num_classes, activation=nn.LeakyReLU, norm=nn.BatchNorm2d, init_out_channels=64, image_channels=3, 
+                 init_image_size=64, image_pool=nn.AvgPool2d(2, stride=2)):
+        """
+        Initialize encoder, consisting of several encoder blocks
+        
+        Args:
+            num_classes: Number of possible classes
+            activation: Activation function, f, to use
+            norm: Normalization function to use inside encoder block
+            init_out_channels: The number of channels the output of the first encoder block should output
+            image_channels: Number of channels for the input images
+            init_image_size: The initial size of the images fed into the MRUs
+            image_pool: Pooling function to halve size of images fed into MRUs
+        """
+        out1 = init_out_channels
+        out2 = out1 * 2
+        out4 = out2 * 2
+        out8 = out4 * 2
+        
+        self.layer1 = EncoderBlock(1, out1, image_channels, activation, norm(out1))
+        self.layer2 = EncoderBlock(out1, out2, image_channels, activation, norm(out2))
+        self.layer3 = EncoderBlock(out2, out4, image_channels, activation, norm(out4))
+        self.layer4 = EncoderBlock(out4, out8, image_channels, activation, norm(out8))
+        self.label_embeds = nn.Embedding(num_classes, init_image_size ** 2)
+        self.init_image_size = init_image_size
+        self.image_pool = image_pool
+    
+    def forward(self, labels, images):
+        """
+        Implements forward pass of encoder
+        
+        Args:
+            labels: Tensor of integer labels for each image of size (batch_size, 1)
+            images: Tensor of images of size (batch_size, image_channels, initial_image_size, initial_image_size)
+            
+        Returns:
+            out: Output of encoder of size (batch_size, init_out_channels * 8, height / 8, width / 8)
+        """
+        embeds = self.label_embeds(labels)
+        embeds = embeds.view(-1, init_image_size, init_image_size)
+        layer1_out = self.layer1(embeds, images)
+        images = self.image_pool(layer1_out)
+        layer2_out = self.layer2(layer1_out, images)
+        images = self.image_pool(layer2_out)
+        layer3_out = self.layer3(layer2_out, images)
+        images = self.image_pool(layer3_out)
+        layer4_out = self.layer4(layer3_out, images)
+        return layer4_out
+        
 
 class sketchGAN_G(nn.Moduel):
     def __init__(self, mru, layer_dims, sampling_dims):
