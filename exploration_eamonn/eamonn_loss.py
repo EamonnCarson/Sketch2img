657fcd3089e198895bf636bdec3370e8f17ac413
Eamonn Carson <eamonncarson@gmail.com>
2019-05-03 13:32:43 -0700

    comments on eamonn_loss.py

--- exploration_eamonn/eamonn_loss.py
+++ exploration_eamonn/eamonn_loss.py
@@ -1,34 +1,19 @@
 import torch
 import torchvision
-import numpy as np
-import scipy
-
-# code adapted from https://github.com/jfsantos/dragan-pytorch/blob/master/dragan.py
-
-def gan_generator_loss(discriminator, generator, real_image_sample, sketch_sample):
-    # first evaluate loss on real images (for discriminator)
-    fake_image_sample = generator.forward(sketch_sample)
-    pred_fake = discriminator.forward(fake_image_sample)
-    labels = torch.ones(pred_fake.size) # ones because we want gen to fool discrim
-    loss_fake = loss_func(pred_fake, labels)
-    return loss_fake
-
-def ac_generator_loss(discriminator, generator, real_image_sample, sketch_sample):
-    ac_loss = ce_loss(pred_fake_class, class_labels)
 
 def discriminator_loss(discriminator, real_image_sample, fake_image_sample, class_labels, lambda_=10, k_=1):
     bce_loss = torch.nn.BCELoss()
     ce_loss = torch.nn.CrossEntropyLoss()
 
     # Generate basic predictions
-    pred_real = discriminator.forward(real_image_sample)
-    pred_fake = discriminator.forward(fake_image_sample)
-    pred_real_natural = pred_real[:, 0]
+    pred_real = discriminator.forward(real_image_sample) # predictions on real images
+    pred_fake = discriminator.forward(fake_image_sample) # predictions on fake images
+    pred_real_natural = pred_real[:, 0] # prediction of real or fake
     pred_fake_natural = pred_fake[:, 0]
-    pred_real_class = pred_real[:, 0:]
+    pred_real_class = pred_real[:, 0:] # prediction of which class
     pred_fake_class = pred_fake[:, 0:]
 
-    # GAN Loss
+    ## GAN Loss
     # first evaluate loss on real images (for discriminator)
     labels = torch.ones(pred_real_natural.size)
     loss_real = bce_loss(pred_real_natural, labels)
@@ -36,6 +21,7 @@ def discriminator_loss(discriminator, real_image_sample, fake_image_sample, clas
     labels = torch.zeros(pred_fake_natural.size)
     loss_fake = bce_loss(pred_fake_natural, labels)
     # third do DRAGAN gradient penalty
+    # code adapted from https://github.com/jfsantos/dragan-pytorch/blob/master/dragan.py
     alpha = torch.rand(real_image_sample, 1)
     x_hat = Variable(alpha * real_image_sample + (1 - alpha) * (real_image_sample + 0.5 * real_image_sample.std() * torch.rand(real_image_sample.size())), requires_grad=True)
     pred_hat = discriminator.forward(x_hat)
@@ -45,7 +31,7 @@ def discriminator_loss(discriminator, real_image_sample, fake_image_sample, clas
     gradient_penalty = lambda_ * ((gradients.norm(2, dim=1) - k) ** 2).mean()
     gan_loss = loss_real + loss_fake + gradient_penalty
 
-    # AC Loss
+    ## AC Loss
     ac_loss = ce_loss(pred_real_class, class_labels)
 
     return ac_loss + gan_loss
