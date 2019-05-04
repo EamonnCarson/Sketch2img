4e8b32e93728d88d280f377e7a5349660bf4d9cd
dongsubkim <ds.kim@berkeley.edu>
2019-05-03 18:50:12 -0700

    created final folder

---
+++ final/mru.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+
+class MRU(nn.Module):
+    """
+    Masked Residual Unit, which takes in feature maps x_i and an image I, and outputs new feature maps y_i
+    """
+    def _conv3x3(self, in_channels, out_channels, norm, activation):
+        """
+        Implements 3x3 convolution, normalization, and activation in MRU unit.
+        
+        Args:
+            in_channels: Number of input channels (e.g. 3 for a RGB image)
+            out_channels: Number of output channels
+            norm: Normalization function to use (without num_features specified). Can be None for no norm.
+            activation: Activation function to use after convolution
+        """
+        # Padding of 1 ensures that input size = output size
+        conv = nn.Conv2d(in_channels, out_channels, 3, padding=1)
+#         torch.nn.init.xavier_uniform_(conv.weight)
+        torch.nn.init.normal_(conv.weight, std=0.02)
+        if self.sn:
+            conv = torch.nn.utils.spectral_norm(conv)
+        ml = nn.ModuleList([conv])
+        
+        if norm:
+            ml.append(norm(out_channels))  # Give it correct num_features, then feed in input
+        ml.append(activation)
+        return ml
+    
+    def __init__(self, in_channels, out_channels, image_channels, norm=nn.BatchNorm2d, mask_norm=False,
+                 activation=nn.ReLU(), mask_activation=nn.Sigmoid(), sn=False):
+        """
+        Args:
+            in_channels: Number of input channels (e.g. 3 for a RGB image)
+            out_channels: Number of output channels
+            image_channels: Number of channels for the input image
+            norm: Normalization function to use after convolution (without num_features specified)
+            mask_norm: True if norm function should also be applied to masks (mi and ni)
+            activation: Activation function, f, to use
+            mask_activation: Activation function to use on masks mi and ni
+            sn: True if spectral normalization is used for convolution weights (mainly for discriminator)
+        """
+        super(MRU, self).__init__()
+        mask_norm = norm if mask_norm else None
+        self.sn = sn
+        self.conv_mi = self._conv3x3(in_channels + image_channels, in_channels, mask_norm, mask_activation)
+        self.conv_ni = self._conv3x3(in_channels + image_channels, out_channels, mask_norm, mask_activation)
+        self.conv_zi = self._conv3x3(in_channels + image_channels, out_channels, norm, activation)
+        self.conv_xi = self._conv3x3(in_channels, out_channels, norm, activation)
+
+    def forward(self, xi, image):
+        """
+        Args:
+            xi: Input feature map of size (batch_size, in_channels, height, width)
+            image: Input images of size (batch_size, sketch_channels, height, width)
+            
+        Returns:
+            yi: Output feature map of size (batch_size, out_channels, height, width)
+        """
+        xi_image_concat = torch.cat((xi, image), dim=1)  # Concat on channel dimension
+    
+        mi = self.conv_mi(xi_image_concat)
+        mask_concat = torch.cat((mi * xi, image), dim=1)
+        zi = self.conv_zi(mask_concat)
+        
+        ni = self.conv_ni(xi_image_concat)
+        xi_new = self.conv_xi(xi)
+        
+        yi = (1 - ni) * xi_new + ni * zi
+        return yi
---
+++ final/train.py
@@ -0,0 +1,177 @@
+"""
+Training process of SketchyGAN
+Mostly adapted from https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
+"""
+
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from generator import *
+from discriminator import *
+import torch.optim as optim
+import time
+import matplotlib.pyplot as plt
+from utils import * 
+from loss import *
+
+
+#PARAMS
+
+# Learning rates for discriminator and generator based on the SketchyGAN paper
+d_lr = 0.0002
+g_lr = 0.0001
+
+num_epochs = 5
+batch_size = 8
+
+
+# TODO: implement photo_sketch_dl 
+photo_sketch_dl = None
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+netG = Generator(num_classes=125,
+                 activation=nn.ReLU,
+                 norm=nn.BatchNorm2d,
+                 init_out_channels=64,
+                 image_channels=3,
+                 init_image_size=64
+                 ).to(device)
+
+netD = Discriminator(num_classes=125
+                     activation=nn.LeakyReLU(negative_slope=0.1, inplace=True),
+                     norm=nn.BatchNorm2d,
+                     init_out_channels=64,
+                     image_channels=3,
+                     init_image_size=64
+                     ).to(device)
+
+dis_criterion = nn.BCEWithLogitsLoss()
+aux_criterion = nn.CrossEntropyLoss()
+
+# import supervision, perceptual, and divsersity loss
+spd_loss = None
+
+real_label = 1.0
+fake_label = 0.0
+
+
+optimizerD = optim.Adam(netD.parameters(), lr=d_lr)
+optimizerG = optim.Adam(netG.parameters(), lr=g_lr)
+# TODO: Maybe add schedulers for optimizers?
+
+G_losses = []
+D_losses = []
+img_list = []
+iters = 0
+acc_list = []
+
+print("Begin training ...")
+begin_time = time.time()
+for epoch in range(num_epochs):
+    for i, data in enumerate(photo_sketch_dl):
+        """
+        Loss function(criterion) returns sum of 
+        GANLoss - auxiliary loss + supervision loss + perceptual loss + diversity loss
+        """
+        # Assuming data is a list of [photos, labels, sketches]
+        input_photos, input_sketches, input_labels = data
+        
+        ## Update netD: maximize log(D(y)) + log(1-D(G(x,z)))
+        # Train with all-real images
+        netD.zero_grad()
+        # Format batch
+        dis_label = torch.full((batch_size, ), real_label, device=device)
+        aux_label = torch.full((batch_size, ), input_labels, device=device)
+        # Forward pass real batch thru D
+        dis_output_real, aux_output_real = netD(input_photos)
+        # Calculate loss on all-real batch
+        dis_errD_real = dis_criterion(dis_output_real, dis_label) #pred_real_natural
+        aux_errD_real = aux_criterion(aux_output_real, aux_label) #pred_real_class
+        # Calculate gradients for D in the backward pass
+        errD_real = dis_errD_real + aux_errD_real
+        errD_real.backward()
+        D_x = dis_output.mean().item()
+        
+        # compute the current classification accuracy
+        accuracy = compute_acc(aux_output, aux_label)
+        acc_hist.append(accuracy)
+
+        ## Train with all-fake batch
+        # Generate fake image batch with G 
+        # Generator outputs generated image and noise vector applied on the bottleneck
+        fake, z = netG(input_labels, input_sketches)
+        dis_label.data.fill_(fake_label)
+        # Classify all fake batch with D
+        dis_output_fake, aux_output_fake = netD(fake)
+        # Calculate D's loss on the all-fake batch
+        dis_errD_fake = dis_criterion(dis_output_fake, dis_label) #pred_fake_natural
+        aux_errD_fake = aux_criterion(aux_output_fake, aux_label) #pred_fake_class
+        # Calculate the gradients for this batch
+        errD_fake = dis_errD_fake + aux_errD_fake
+        errD_fake.backward()
+        D_G_z1 = dis_output.mean().item()
+        # Calculate DRAGAN Loss
+        grad_penalty = gradient_penalty(netD, input_photos, fake)
+        grad_penalty.backward()
+        # Add the gradients from the all-real and all-fake batches
+        errD = errD_real + errD_fake + grad_penalty
+        # Update D
+        optimizerD.step()
+
+        ## Update G Network: maximize log(D(G(x,z)))
+        netG.zero_grad()
+        dis_label.data.fill_(real_label) #fake labels are real label for generator
+        # Since we just updated D, perform another forward pass of all-fake batch thru D
+        dis_output, aux_output = netD(fake)
+        # Calculate G's loss based on this output
+        dis_errG = dis_criterion(dis_output, dis_label)
+        aux_errG = aux_criterion(aux_output, aux_label)
+        # Calculate gradients for G
+        # Supervised loss
+        supervised_loss_G = supervised_loss(fake, input_photos)
+        # Perceptual loss
+        perceptual_loss_G = perceptual_loss(fake, input_photos)
+        # Diversity Loss; Create another fake image
+        fake_alt , z_alt = netG(input_labels, input_sketches)
+        diversity_loss_G = diversity_loss(fake, fake_alt, z, z_alt)
+        errG = dis_errG - aux_errG + supervised_loss_G + perceptual_loss_G + diversity_loss_G
+        errG.backward()
+        D_G_z2 = dis_output.mean().item()        
+        # Update G
+        optimizerG.step()
+
+        # Print trainig stats
+        if i % 50 == 0:
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                % (epoch, num_epochs, i, len(photo_sketch_dl),
+                errD.item(), errG.item(), D_x, D_G_z1, D_G_z2)
+                )
+        
+        # Save Losses for plotting later
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
+        
+        # Save a sketch and corresponding photo
+        if (iters % 500 == 0) or ((epoch == num_epochs - 1) and (i == len(photo_sketch_dl) - 1)):
+            img_list.append((fake[0], input_photos[0]))
+
+                
+print("Training ended.")
+print("Time took : ", time.time()-begin_time)
+
+
+# Plot losses
+plt.figure(figsize=(10,5))
+plt.title("Generator and Discriminator Loss During Training")
+plt.plot(G_losses,label="G")
+plt.plot(D_losses,label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
+# Plot sketches and photos pairs saved during the training
